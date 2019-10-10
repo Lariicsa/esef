34f7ee659e89eb76f46d58817cc8ccf6f816b3c2
Lariicsa <dejamepienso@gmail.com>
2019-10-09 23:06:12 -0500

    set layout Student detail

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -17,6 +17,13 @@ export default class Layout extends Component {
                 {this.props.children}
               </section>
             </div>
+            <footer className="footer">
+              <div className="columns is-centeredentered is-vc">
+                <div className="column is-12">
+                <p className="has-text-centered">Made with love, tears & fun in Ironhack by Larissa Avila</p>
+                </div>
+              </div>
+            </footer>
           </>
         )
         }
